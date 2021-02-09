76e6049b8437fbb8320784224d59eccdca56d550
Mathias Hertlein <mathias@mh.cx>
2021-02-09 13:56:03 +0100

    We're running into strange errors, if we use one prepared statement for all - next try

--- src/main/kotlin/measurements/PostgresMeasurementRepository.kt
+++ src/main/kotlin/measurements/PostgresMeasurementRepository.kt
@@ -1,23 +1,35 @@
 package measurements
 
+import com.impossibl.postgres.jdbc.PGSQLSimpleException
 import java.sql.Connection
-import java.sql.PreparedStatement
 import java.sql.Timestamp
 import java.util.*
 
 class PostgresMeasurementRepository(private val db: Connection) : MeasurementRepository {
 
+    init{
+        db.nativeSQL("DISCARD ALL");
+    }
+
+    private val insertMeasurementStatement = db.prepareStatement(
+        "INSERT INTO measurements(measurement_id, sensor_id, measurement_date, measurement_value) VALUES (?, ?, ?, ?)"
+    )
+
     override fun addMeasurement(measurement: Measurement) {
-        val insertMeasurementStatement = db.prepareStatement(
-            "INSERT INTO measurements(measurement_id, sensor_id, measurement_date, measurement_value) VALUES (?, ?, ?, ?)"
-        )
 
         insertMeasurementStatement.setString(1, UUID.randomUUID().toString())
         insertMeasurementStatement.setString(2, measurement.sensorId.toString())
         insertMeasurementStatement.setTimestamp(3, Timestamp.from(measurement.measurementDate))
         insertMeasurementStatement.setDouble(4, measurement.value)
-        insertMeasurementStatement.executeUpdate()
+        try {
+            insertMeasurementStatement.executeUpdate()
+        } catch (e: PGSQLSimpleException) {
+            val parts = e.message?.split('"') ?: throw e
+            if (parts.size != 3) {
+                throw e
+            }
 
-        insertMeasurementStatement.close()
+            db.nativeSQL("DEALLOCATE " + parts[1])
+        }
     }
 }
