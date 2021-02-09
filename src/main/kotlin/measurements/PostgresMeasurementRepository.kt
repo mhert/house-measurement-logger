36f461ad284d2b5089a9066c67ce70db87455bbf
Mathias Hertlein <mathias@mh.cx>
2021-02-09 13:13:17 +0100

    We're running into strange errors, if we use one prepared statement for all

--- src/main/kotlin/measurements/PostgresMeasurementRepository.kt
+++ src/main/kotlin/measurements/PostgresMeasurementRepository.kt
@@ -6,15 +6,18 @@ import java.sql.Timestamp
 import java.util.*
 
 class PostgresMeasurementRepository(private val db: Connection) : MeasurementRepository {
-    private val insertMeasurementStatement: PreparedStatement = db.prepareStatement(
-        "INSERT INTO measurements(measurement_id, sensor_id, measurement_date, measurement_value) VALUES (?, ?, ?, ?)"
-    )
 
     override fun addMeasurement(measurement: Measurement) {
+        val insertMeasurementStatement = db.prepareStatement(
+            "INSERT INTO measurements(measurement_id, sensor_id, measurement_date, measurement_value) VALUES (?, ?, ?, ?)"
+        )
+
         insertMeasurementStatement.setString(1, UUID.randomUUID().toString())
         insertMeasurementStatement.setString(2, measurement.sensorId.toString())
         insertMeasurementStatement.setTimestamp(3, Timestamp.from(measurement.measurementDate))
         insertMeasurementStatement.setDouble(4, measurement.value)
         insertMeasurementStatement.executeUpdate()
+
+        insertMeasurementStatement.close()
     }
 }
