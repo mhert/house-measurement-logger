d323f2a96c7b45925111b4556ed3175c78fd2f2f
Mathias Hertlein <mathias@mh.cx>
2021-02-10 10:06:34 +0100

    Prevent duplicate primary keys

--- src/main/kotlin/measurements/PostgresMeasurementRepository.kt
+++ src/main/kotlin/measurements/PostgresMeasurementRepository.kt
@@ -15,12 +15,23 @@ class PostgresMeasurementRepository(private val db: Connection) : MeasurementRep
         "INSERT INTO measurements(measurement_id, sensor_id, measurement_date, measurement_value) VALUES (?, ?, ?, ?)"
     )
 
-    override fun addMeasurement(measurement: Measurement) {
+    private val findMeasurementStatementById = db.prepareStatement(
+        "SELECT * FROM measurements WHERE measurement_id = ?"
+    )
 
-        insertMeasurementStatement.setString(1, UUID.randomUUID().toString())
+    @Synchronized override fun addMeasurement(measurement: Measurement) {
+
+        var measurementId: UUID
+
+        do {
+             measurementId = UUID.randomUUID()
+        } while (existsMeasurementWithId(measurementId))
+
+        insertMeasurementStatement.setString(1, measurementId.toString())
         insertMeasurementStatement.setString(2, measurement.sensorId.toString())
         insertMeasurementStatement.setTimestamp(3, Timestamp.from(measurement.measurementDate))
         insertMeasurementStatement.setDouble(4, measurement.value)
+
         try {
             insertMeasurementStatement.executeUpdate()
         } catch (e: PGSQLSimpleException) {
@@ -31,5 +42,23 @@ class PostgresMeasurementRepository(private val db: Connection) : MeasurementRep
 
             db.nativeSQL("DEALLOCATE " + parts[1])
         }
+    }
+
+    fun existsMeasurementWithId(measurementId: UUID): Boolean {
+
+        findMeasurementStatementById.setString(1, measurementId.toString())
+
+        try {
+            findMeasurementStatementById.execute()
+        } catch (e: PGSQLSimpleException) {
+            val parts = e.message?.split('"') ?: throw e
+            if (parts.size != 3) {
+                throw e
+            }
+
+            db.nativeSQL("DEALLOCATE " + parts[1])
+        }
+
+        return findMeasurementStatementById.resultSet.fetchSize == 0
     }
 }
