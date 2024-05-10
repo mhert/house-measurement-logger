f0a54b828a4c16b54d9abdb95fca4bbfa5eb98a7
Mathias Hertlein <mathias@mh.cx>
2024-05-10 18:54:24 +0200

    Upgrade some libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,16 +26,16 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.calimero:calimero-core:2.4")
-    implementation("com.github.calimero:calimero-device:2.4")
-    implementation("com.github.calimero:calimero-rxtx:2.4")
-    implementation("com.ghgande:j2mod:2.7.0")
+    implementation("com.github.calimero:calimero-core:2.5.1")
+    implementation("com.github.calimero:calimero-device:2.5.1")
+    implementation("com.github.calimero:calimero-rxtx:2.5.1")
+    implementation("com.ghgande:j2mod:3.2.1")
     implementation("io.ktor:ktor-client-core:1.6.4")
     implementation("io.ktor:ktor-client-cio:1.6.4")
     implementation("io.ktor:ktor-client-serialization:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
     implementation("com.impossibl.pgjdbc-ng", "pgjdbc-ng", "0.8.9")
-    runtimeOnly("org.slf4j:slf4j-simple:1.8.0-beta4")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.13")
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
