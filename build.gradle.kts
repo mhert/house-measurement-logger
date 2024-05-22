7cdcbb14c7f4afcbef33dcd2505b1fda78d774cb
Mathias Hertlein <mathias@mh.cx>
2024-05-22 16:59:51 +0200

    Clean up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,44 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val calimero = "3.0-SNAPSHOT"
+val j2mod = "3.2.1"
+val junitJupiter = "5.6.0"
+val kotlinxSerializationJson = "1.0.1"
+val ktor = "1.6.4"
+val pgjdbcNg = "0.8.9"
+
 plugins {
+    id("com.diffplug.spotless") version "6.25.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.2.5"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.serialization") version "1.9.24"
-    id("org.springframework.boot") version "3.2.5"
-    id("io.spring.dependency-management") version "1.1.4"
-    id("com.diffplug.spotless") version "6.25.0"
 }
 
-sourceSets { main { dependencies { implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6") } } }
-
-group = "me.mhert"
-
-version = "1.0-SNAPSHOT"
+dependencies {
+    implementation("com.ghgande", "j2mod", j2mod)
+    implementation("com.impossibl.pgjdbc-ng", "pgjdbc-ng", pgjdbcNg)
+    implementation("io.calimero", "calimero-core", calimero)
+    implementation("io.calimero", "calimero-device", calimero)
+    implementation("io.ktor", "ktor-client-cio", ktor)
+    implementation("io.ktor", "ktor-client-core", ktor)
+    implementation("io.ktor", "ktor-client-serialization", ktor)
+    implementation("org.jetbrains.kotlin", "kotlin-reflect")
+    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", kotlinxSerializationJson)
+    implementation("org.springframework.boot", "spring-boot-starter")
+    runtimeOnly("io.calimero", "calimero-rxtx", calimero)
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", junitJupiter)
+    testImplementation("org.springframework.boot", "spring-boot-starter-test")
+    testImplementation(kotlin("test-junit5"))
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", junitJupiter)
+}
 
 repositories {
     mavenCentral()
-    jcenter()
 
     maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
     maven(url = "https://kotlin.bintray.com/kotlinx")
-}
-
-val calimeroVersion = "3.0-SNAPSHOT"
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("io.calimero:calimero-core:${calimeroVersion}")
-    implementation("io.calimero:calimero-device:${calimeroVersion}")
-    implementation("com.ghgande:j2mod:3.2.1")
-    implementation("io.ktor:ktor-client-core:1.6.4")
-    implementation("io.ktor:ktor-client-cio:1.6.4")
-    implementation("io.ktor:ktor-client-serialization:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("com.impossibl.pgjdbc-ng", "pgjdbc-ng", "0.8.9")
-    runtimeOnly("io.calimero:calimero-rxtx:${calimeroVersion}")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
 spotless {
@@ -71,3 +69,7 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<Test> { useJUnitPlatform() }
+
+group = "me.mhert"
+
+version = "1.0-SNAPSHOT"
