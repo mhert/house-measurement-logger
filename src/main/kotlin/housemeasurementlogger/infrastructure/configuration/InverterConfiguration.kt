927bb3c2b86907816c50b5380a33d2659e108c32
Mathias Hertlein <mathias@mh.cx>
2025-06-02 21:57:07 +0200

    Inject HttpClient into Inverter via DI

--- src/main/kotlin/housemeasurementlogger/infrastructure/configuration/InverterConfiguration.kt
+++ src/main/kotlin/housemeasurementlogger/infrastructure/configuration/InverterConfiguration.kt
@@ -4,6 +4,10 @@ import housemeasurementlogger.inverter.FileBasedInverterSensorsRepository
 import housemeasurementlogger.inverter.HttpBasedInverter
 import housemeasurementlogger.inverter.Inverter
 import housemeasurementlogger.inverter.InverterSensorsRepository
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -19,6 +23,20 @@ class InverterConfiguration {
 
     @Bean
     fun inverter(config: HouseMeasurementLoggerConfigProperties): Inverter {
-        return HttpBasedInverter(config.inverterBaseUrl)
+        val client =
+            HttpClient(CIO) {
+                install(JsonFeature) {
+                    serializer =
+                        KotlinxSerializer(
+                            kotlinx.serialization.json.Json {
+                                prettyPrint = true
+                                isLenient = true
+                                ignoreUnknownKeys = true
+                            }
+                        )
+                }
+            }
+
+        return HttpBasedInverter(client, config.inverterBaseUrl)
     }
 }
--- src/main/kotlin/housemeasurementlogger/inverter/HttpBasedInverter.kt
+++ src/main/kotlin/housemeasurementlogger/inverter/HttpBasedInverter.kt
@@ -1,27 +1,12 @@
 package housemeasurementlogger.inverter
 
 import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 
-class HttpBasedInverter(private val inverterBaseUrl: String) : Inverter {
-    override suspend fun instantData(): InstantData {
-        val client =
-            HttpClient(CIO) {
-                install(JsonFeature) {
-                    serializer =
-                        KotlinxSerializer(
-                            kotlinx.serialization.json.Json {
-                                prettyPrint = true
-                                isLenient = true
-                                ignoreUnknownKeys = true
-                            }
-                        )
-                }
-            }
+class HttpBasedInverter(private val client: HttpClient, private val inverterBaseUrl: String) :
+    Inverter {
 
+    override suspend fun instantData(): InstantData {
         val response: HttpBasedInverterResponse =
             client.get("$inverterBaseUrl/solar_api/v1/GetPowerFlowRealtimeData.fcgi")
 
